4fc9fde5d7041e64bfb3b26321813389b0c8afc9
Jesús Federico Franco <j.francomedinilla@gmail.com>
2017-02-24 17:59:28 +0100

    pre-master añadido

--- config/db.php
+++ config/db.php
@@ -1,9 +1,28 @@
 <?php
 
+$url = getenv('DATABASE_URL');
+
+if ($url !== false) {
+    $matches = parse_url($url);
+    $host = $matches['host'];
+    $port = $matches['port'];
+    $dbname = substr($matches['path'], 1);
+    $username = $matches['user'];
+    $password = $matches['pass'];
+
+    return [
+        'class' => 'yii\db\Connection',
+        'dsn' => "pgsql:host=$host;port=$port;dbname=$dbname",
+        'username' => $username,
+        'password' => $password,
+        'charset' => 'utf8',
+    ];
+}
+
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'pgsql:host=localhost;dbname=animas',
+    'username' => 'animas',
+    'password' => 'animas',
     'charset' => 'utf8',
 ];
